dfe38acdbd9df6fffd4e75d1cf7c1a0b5c9dbe65
Michael Armes <michael@un-loop.org>
2019-04-15 09:51:26 -0700

    move to new version of unloop-resource-builder

--- server/server.js
+++ server/server.js
@@ -2,16 +2,14 @@ const path = require('path');
 const koa = require('koa');
 const mount = require('koa-mount');
 const body = require('koa-bodyparser');
-const convert = require('koa-convert');
-const nestedRouter = require('koa-recursive-resource-router');
 const query = require('unloop-koa-query');
 const decode = require('koa-decode-params');
 const cryptKey = process.env.npm_config_cryptKey;
 const crypt = require('unloop-crypt')(cryptKey);
+const Auth = require('koa-auth-wrapper');
 
 const session = require('./session');
 const userEntity = require("./entity/users");
-const Auth = require('koa-auth-wrapper');
 const context = require('./user-context');
 const resourceBuilder = require('unloop-resource-builder')(__dirname);
 const staticRouter = require('unloop-static-router')( path.resolve(__dirname, "../client"),
@@ -35,9 +33,7 @@ const staticRouter = require('unloop-static-router')( path.resolve(__dirname, ".
     ]
 );
 
-const queryMiddleware = convert.back(query());
-const decodeMiddleware = convert.back(decode());
-const builderWithMiddleware = resourceBuilder(decodeMiddleware, queryMiddleware);
+const builderWithMiddleware = resourceBuilder(query(), decode());
 
 const events = builderWithMiddleware('events');
 const attendees = builderWithMiddleware('attendees');
@@ -45,7 +41,7 @@ const users = builderWithMiddleware('users');
 events.add(attendees);
 
 const koaApi = new koa();
-koaApi.use(nestedRouter(events));
+koaApi.use(events.middleware());
 koaApi.use(users.middleware());
 
 const koaApp = new koa();
@@ -53,7 +49,13 @@ const koaApp = new koa();
 koaApp.use(body());
 
 koaApp.use(session(koaApp, cryptKey));
-koaApp.use(context());
+koaApp.use(context( (user) => ({ // payload must hold only UI concerns, this is exposed to the browser and is not secure
+        username: user ? user.username : "",
+        name: user ?  user.name : "",
+        isAdmin: user ? user.roles.findIndex((item) => item === "super" || item === "admin" ) !== -1 : false,
+        isLoggedIn: Boolean(user)
+    })
+))
 
 const auth = new Auth({
     login: "/login",
--- server/user-context.js
+++ server/user-context.js
@@ -3,20 +3,13 @@ const defaultConfig = {
     signed: false
 };
 
-module.exports = (config) => {
+module.exports = (payloadCallback, config) => {
     if (!config) config = defaultConfig;
 
     return async (ctx, next) => {
         await next();
 
-        let payload = { // payload must hold only UI concerns, this is exposed to the browser and is not secure
-            username: ctx.req.user ? ctx.req.user.username : "",
-            name: ctx.req.user ?  ctx.req.user.name : "",
-            isAdmin: ctx.req.user ? ctx.req.user.roles.findIndex((item) => item === "super" ) !== -1 : false,
-            isLoggedIn: Boolean(ctx.req.user)
-        }
-
+        let payload = payloadCallback(ctx.req.user);
         ctx.cookies.set("user-context",JSON.stringify(payload), config);
-
     }
 }
